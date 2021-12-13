4c5fd52f8f08b22215a03738694101cb2a4ba424
Matheus Maricondi <maricondiwielki@gmail.com>
2021-12-12 23:28:42 -0300

    master - added api get back dada

--- src/pages/weather/Weather.tsx
+++ src/pages/weather/Weather.tsx
@@ -1,21 +1,33 @@
 import { useState, useEffect, useContext } from 'react'
 import { weatherResponse, locationResponse } from './types'
+import api from '../../services/api'
+import { apiConstants } from '../../constants/api'
 import LoadingContext from '../../context/loading'
-import { useFetchWeather } from '../../hooks/useFetchWeather'
-
 
 const Weather = () => {
-    // const [weatherData, fetchWeather] = useFetchWeather()
     const { state, setState } = useContext(LoadingContext)
+    const [geoLocation, setGeoLocation] = useState<locationResponse>()
+    const [weatherData, setWeatherData] = useState<weatherResponse>()
+    const [toast, setToast] = useState('')
 
     useEffect(() => {
         setState({ loading: true })
         navigator.geolocation.getCurrentPosition(local => {
             console.log(local.coords)
-            setState({ loading: false })
+            setGeoLocation(local.coords)
         })
     }, [])
-
+    useEffect(() => {
+        if (geoLocation) {
+            api.get<weatherResponse>(`/onecall?lat=${geoLocation?.latitude}&lon=${geoLocation?.longitude}&exclude=hourly,daily&appid=${apiConstants.api_key}`).then(res => {
+                console.log(res.data)
+                setWeatherData(res.data)
+                setState({ loading: false })
+            }).catch(err => {
+                setToast('Get weather fail')
+            })
+        }
+    }, [geoLocation])
 
     return (
         <div>
